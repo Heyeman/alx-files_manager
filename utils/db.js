830b1d6d7d9c64c9f342e0f3da87bcddab97bd4b
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-06-11 13:56:44 +0300

    mongo driver

---
+++ utils/db.js
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    this.client = MongoClient(`mongodb://${host}:${port}`, {
+      useUnifiedTopology: true,
+    });
+    this.client.connect(() => {
+      this.db = this.client.db(database);
+      console.log();
+    });
+  }
+
+  isAlive() {
+    if (this.db) return true;
+    return false;
+  }
+
+  async nbUsers() {
+    const usersCollection = this.db.collection('users');
+    return usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    const files = this.db.collection('files');
+    return files.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
